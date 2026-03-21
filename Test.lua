0f1e98ecbd8d4439aed3a9e834aaea8afc114b78
iansutherlandmusic <iansutherlandmusic@gmail.com>
2026-03-20 22:59:52 -0400

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,15 @@
+-- Welcome to JDoodle!
+--
+-- You can execute code here in 88 languages. Right now you’re in the Lua IDE. 
+--
+--  1. Click the orange Execute button ️▶ to execute the sample code below and see how it works.
+--  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+--  3. Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+--
+-- Want to change languages? Try the search bar up the top.
+
+local x = 10
+local y = 25
+local z = x + y
+
+print("Sum of x+y =",z)
